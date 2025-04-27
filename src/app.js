38c65f48192bec23e391f7847413be59badffc26
Praful Gahlot <139745050+Praful-bit@users.noreply.github.com>
2025-04-27 15:57:51 +0530

    understanding routing

--- src/app.js
+++ src/app.js
@@ -3,14 +3,29 @@ const express = require("express");
 
 const app = express()
 
+app.use('/hello/0/2',(req, res)=>{
+    res.send("Avra ka dabra gili gili chuuuu !!")
+})
+
+
+app.use('/hello/2',(req, res)=>{
+    res.send("Avra ka dabra !!")
+})
+
+
 app.use('/hello',(req, res)=>{
     res.send("Hello from the Hello")
 })
 
-app.use('/',(req, res)=>{
+
+app.use('/test',(req, res)=>{
+    res.send("Hello from the test server")
+})
+
+app.use('/',(req, res)=>{  //wild card (any thing when match after the / it gives response hello from the server deshboard)
     res.send("Hello from the server deshboard ")
 })
 
-app.listen(3000,()=>{
+app.listen(7777,()=>{
     console.log("Server is listening on the port 3000")
 })
