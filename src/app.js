0fed4e6fc32abcbf815bcc7cf5ff7c2cc581d296
Praful Gahlot <139745050+Praful-bit@users.noreply.github.com>
2025-04-29 21:00:44 +0530

    new way routes handlers

--- src/app.js
+++ src/app.js
@@ -5,28 +5,23 @@ const app = express();
 //app.user('/routes', rh,[rh2.rh3],rh4,rh5)
 
 app.get(
-  "/user",[
+    "/user",
+    (req, res, next) => {
+      console.log("hey here 2 responce")
+      res.send("Responce 2!!");
+    },
+  );
+
+app.get(
+  "/user",
   (req, res, next) => {
-    console.log("hey here no any responce")
-    next()
+    console.log("hey here 1 responce")
     // res.send("Responce !!");
-  },
-  (req, res, next) => {
-    console.log("here is 2 responce")
-    // res.send("hey Responce 2!");
-    next()
-  }],
-  (req, res, next) => {
-    console.log("here is 3 responce")
-    // res.send("hey Responce 3!");
     next()
   },
-  (req, res, next) => {
-    console.log("here is 4 responce")
-    res.send("hey Responce 4!");
-    next()
-  }
 );
+
+
 
 app.listen(3000, () => {
   console.log("All things are ok!!");
