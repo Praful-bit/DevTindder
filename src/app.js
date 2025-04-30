331798a458823b7dadc9b67ad84fd9a3cf094880
Praful Gahlot <139745050+Praful-bit@users.noreply.github.com>
2025-04-30 19:10:58 +0530

    express js defination

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+// GET /user ===> it checks all the app.xxx("matching routes") functions  when it find the function that
+// actualy give res back its call express work
+
 //app.user('/routes', rh,[rh2.rh3],rh4,rh5)
 
 app.get(
