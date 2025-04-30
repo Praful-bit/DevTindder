df1994a5db5adf4c63f4eeb60c88a3eb73134d0e
Praful Gahlot <139745050+Praful-bit@users.noreply.github.com>
2025-04-30 20:04:43 +0530

    i make dommy auth for admin and user

--- src/app.js
+++ src/app.js
@@ -2,20 +2,21 @@ const express = require("express");
 
 const app = express();
 
+const {adminAuth,userAuth} = require('./utils/middleWares/auth')
+
 // GET /user ===> it checks all the app.xxx("matching routes") functions  when it find the function that
 // actualy give res back its call express work
 
 //app.user('/routes', rh,[rh2.rh3],rh4,rh5)
 
+// handle auth midleware for all GET request Get, Post
+app.use("/admin",adminAuth)
+
+app.get("/user",userAuth,(req,res)=>{
+  res.send("Here you have no any user data")
+})
 app.get("/admin/getAllData", (req, res, next) => {
-  //logic of check if the request is authorized
-  const token = "xyz";
-  const isAdminAuthorized = token === "xyz1222";
-  if (isAdminAuthorized) {
-    res.send("All Data sent");
-  }else{
-    res.status(401).send("unAuthorized request")
-  }
+  res.send("Get all data");
 });
 
 app.get("/admin/deleteUser", (req, res) => {
