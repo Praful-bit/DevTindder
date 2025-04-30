ec56b9b781d836454e45e17f62fd4db2465f3f1e
Praful Gahlot <139745050+Praful-bit@users.noreply.github.com>
2025-04-30 20:26:51 +0530

    ERROR

--- src/app.js
+++ src/app.js
@@ -2,27 +2,22 @@ const express = require("express");
 
 const app = express();
 
-const {adminAuth,userAuth} = require('./utils/middleWares/auth')
 
-// GET /user ===> it checks all the app.xxx("matching routes") functions  when it find the function that
-// actualy give res back its call express work
 
-//app.user('/routes', rh,[rh2.rh3],rh4,rh5)
-
-// handle auth midleware for all GET request Get, Post
-app.use("/admin",adminAuth)
-
-app.get("/user",userAuth,(req,res)=>{
-  res.send("Here you have no any user data")
-})
-app.get("/admin/getAllData", (req, res, next) => {
-  res.send("Get all data");
+app.get("/getUserData", (req, res) => {
+  // try {
+    throw new Error("ggauyyuttg");
+    res.send("User Data Sent");
+  // } catch (err) {
+  //   res.status(500).send("some error comes pls contact to sever team");
+  // }
 });
 
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deleted a user");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
 });
-
 app.listen(3000, () => {
   console.log("All things are ok!!");
 });
