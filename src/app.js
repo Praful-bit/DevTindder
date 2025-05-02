5d4bfcb406809988df14fb79bcdf6e59c1e762d5
Praful Gahlot <139745050+Praful-bit@users.noreply.github.com>
2025-05-03 01:42:03 +0530

    i set mongoose first run then api

--- src/app.js
+++ src/app.js
@@ -1,21 +1,16 @@
 const express = require("express");
-
+const connectDB = require("./config/database");
 const app = express();
 
-app.get("/getUserData", (req, res) => {
-  // try {
-  throw new Error("ggauyyuttg");
-  res.send("User Data Sent");
-  // } catch (err) {
-  //   res.status(500).send("some error comes pls contact to sever team");
-  // }
-});
+connectDB()
+  .then(() => {
+    console.log("We are Connected Successfully...");
+    app.listen(7777, () => {
+      console.log("All things are ok!!");
+    });
+  })
+  .catch((err) => {
+    console.log(err, "Connection failed");
+  });
 
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("something went wrong");
-  }
-});
-app.listen(3000, () => {
-  console.log("All things are ok!!");
-});
+
--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("")
+    await mongoose.connect("mongodb+srv://Praful:9QDQihF5n0aGDZBq@prafulnode.hwwt9ju.mongodb.net/devTinder")
 };
 
-connectDB().then(()=>{
-    console.log("We are Connected Successfully...")
-}).catch((err)=>{
-console.log(err,"Connection failed")
-})
+module.exports = connectDB;
