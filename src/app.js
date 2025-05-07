31dcbe2c5b9fe668038cfd1f1945f365c779ff4b
Praful Gahlot <139745050+Praful-bit@users.noreply.github.com>
2025-05-07 18:59:22 +0530

    some validation on api

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const user = require("./models/user");
 
 app.use(express.json());
 app.post("/signup", async (req, res) => {
@@ -37,6 +38,31 @@ app.get("/feed", async (req, res) => {
   }
 });
 
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    // {_id:userId}
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user Deleted successfully");
+  } catch (error) {
+    res.status(400).send("Error fetching the user: " + error.message);
+  }
+});
+
+app.patch("/signup", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "before",
+      runValidators: true,
+    });
+    console.log(user);
+    res.send("user updated successfully..!");
+  } catch (error) {
+    res.status(400).send("Error fetching the user: " + error.message);
+  }
+});
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -3,23 +3,42 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
+    minLength: 4,
+    maxlength: 25,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+    trim: true,
   },
   password: {
     type: String,
   },
   age: {
     type: Number,
+    min: 18,
   },
   gender: {
     type: String,
+    validate(value) {
+      if (!["male", "female", "other"].includes(value)) {
+        throw new Error("Enter data is not valid");
+      }
+    },
   },
+  about: {
+    type: String,
+    default: "this is a default about of the user..!"
+  },
+  skills:{
+    type: [String]
+  }
 });
-
 
 module.exports = mongoose.model("user", userSchema);
